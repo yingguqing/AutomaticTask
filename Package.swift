9c28b20b8d67775f190135ce5c6e95003f367246
zhouziyuan <zhouziyuan@xingfeiinc.com>
2021-12-16 18:53:24 +0800

    1

--- Package.swift
+++ Package.swift
@@ -12,9 +12,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .executableTarget(
-            name: "SwiftDemo",
-            dependencies: []),
         .testTarget(
             name: "SwiftDemoTests",
             dependencies: ["SwiftDemo"]),
