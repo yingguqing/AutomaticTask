8f1f3eaaa1650735302b2c9917dea80cc389fb86
yingguqing <yingguqing@gmail.com>
2021-12-17 16:51:25 +0800

    6

--- Package.swift
+++ Package.swift
@@ -12,13 +12,13 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .executableTarget(
             name: "AutomaticTask",
+            dependencies: [],
             resources:[
                 .copy("Module/BingWallpaper/bing-wallpaper.json"),
                 .copy("Module/BingWallpaper/README.md")
-            ],
-            dependencies: []),
+            ]),
         .testTarget(
             name: "AutomaticTaskTests",
             dependencies: ["AutomaticTask"]),
