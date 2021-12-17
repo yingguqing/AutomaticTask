fe1dc5d55e358718c9ed4a7231cb0fa58267dbd7
yingguqing <yingguqing@gmail.com>
2021-12-17 16:57:47 +0800

    8

--- Sources/AutomaticTask/main.swift
+++ Sources/AutomaticTask/main.swift
@@ -1,3 +1,16 @@
 import Foundation
 
-BWNetwork().getWallPaper()
+let bw = BWNetwork()
+bw.getWallPaper()
+
+var index = 0
+while index < 3600 {
+    if bw.finish {
+        break
+    }
+    index += 1
+    sleep(1)
+}
+
+
+print("ddddd")
