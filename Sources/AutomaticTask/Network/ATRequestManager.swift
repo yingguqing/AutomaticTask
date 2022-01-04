909c3866abe5b113d84a13fb70142e9c1436c701
影孤清 <yingguqing@gmail.com>
2022-01-04 08:07:21 +0800

    Update ATRequestManager.swift
    1

--- Sources/AutomaticTask/Network/ATRequestManager.swift
+++ Sources/AutomaticTask/Network/ATRequestManager.swift
@@ -65,7 +65,7 @@ extension NetworkData {
         guard let url = self.url else { return nil }
         var request = URLRequest(url: url)
         // 设置超时时间
-        request.timeoutInterval = 15
+        request.timeoutInterval = 45
         request.httpMethod = method.rawValue
         if let cookies = cookieString {
             request.httpShouldHandleCookies = true
